8961136ff99d3135799f6748674a2322b6914099
rajkumar2k21 <rajkumar@amplework.com>
2026-09-22 11:48:31 +0530

    Related commit(s):- update code

--- web/src/app/loading.tsx
+++ web/src/app/loading.tsx
@@ -3,6 +3,7 @@ export default function Loading() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-paper">
       {/* <BrandSplashLockup /> */}
+      <div className="flex items-center justify-center"></div>
     </div>
   );
 }
